4f117cd4e167b7f1076f3cf0c2ecf03a269d4eb0
peppermint25 <oskars.metr01@gmail.com>
2022-11-10 12:15:35 +0200

    saies

--- app.py
+++ app.py
@@ -1,30 +1,53 @@
 from flask import Flask, render_template, request, jsonify
+from bson.json_util import dumps
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb+srv://mint:<datubaze>@cluster0.aiihewy.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+client = MongoClient("mongodb+srv://mint:datubaze@cluster0.aiihewy.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 myCollection = db.test
 
 
-
 @app.route("/")
 def main():
-    print ("abc")
-  #  cur = mysql.connection.cursor()
- #   cur.execute("""SELECT user, host FROM mysql.user""")
-   # rv = cur.fetchall()
-  #  print (rv)
-    users=[{"id": 1, "name": "john" },
-           {"id": 2, "name": "alise" }]
-    return render_template('index.html',  users=users)
+    print("abc")
+    #  cur = mysql.connection.cursor()
+    #   cur.execute(db)
+    # rv = cur.fetchall()
+    #  print (rv)
+    users = [{"id": 1, "name": "john"},
+             {"id": 2, "name": "alise"}]
+    return render_template('index.html', users=users)
+
 
 @app.route("/show", methods=['GET'])
-def bbox():
-    print ("123")
-    return jsonify(**{"status": 111,})
+def show():
+    names = db.test.find({}, "name")
+    passwords = db.test.find({}, "password")
+
+    return render_template('index.html', name=names, password=passwords)
+
+
+@app.route("/add", methods=['GET','POST'])
+def add():
+    add = {}
+    if request.method == 'POST':
+        add['name'] = request.form['name']
+        add['password'] = request.form['password']
+        db.test.insert_one(add)
+    return render_template('index.html')
+    
+
+
+@app.route("/delete/<id>", methods=['DELETE'])
+def delete(id):
+    client.db.test.delete_one({'_id': ObjectId(id)})
+    resp = jsonify('User deleted successfully!')
+    resp.status_code = 200
+    return resp
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
