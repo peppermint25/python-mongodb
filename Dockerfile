53f6e9d24f622bebc86850534d92d8318b59839f
peppermint25 <oskars.metr01@gmail.com>
2022-11-28 16:08:19 +0200

    Clean up of the code
    The code is cleaned up but the add users function is broken and it keeps adding new rows on refresh instead of when asked

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.10.8
+WORKDIR /var/lib/python
+
+CMD [ "python", "./app.py"]
--- app.py
+++ app.py
@@ -4,42 +4,24 @@ from flask import Flask, jsonify, render_template, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
-print ("abc")
 #client = MongoClient("mongodb+srv://mint:datubaze@cluster0.aiihewy.mongodb.net/?retryWrites=true&w=majority")
 client = MongoClient("mongodb://localhost:27017/")
-client.db.A.insert_one({"name": "aa"})
 
 db = client.test
-print ("kakis")
-print ("suns")
-abc = client.db.A.find_one({})
-print (abc)
-#myCollection = db.test2
-
 
 @app.route("/", methods=["GET"])
 def main():
-    #  cur = mysql.connection.cursor()
-    #   cur.execute(db)
-    # rv = cur.fetchall()
-    #  print (rv)
     users = client.db.lietotaji.find({})
     users = list(users)
-    print (users)
     return render_template('index.html', lietotaji=users)
     
 @app.route("/add", methods=['POST'])
 def add():
-    print("aaaaaaaaaaaa", request.form)
     obj = {}
     obj['name'] = request.form['name']
     obj['password'] = request.form['password']
     lietotajs = client.db.lietotaji.insert_one(obj)
-    ##return render_template('index.htmml')
     obj['_id'] = str(lietotajs.inserted_id)
-    print ("add", obj)
-    print ()
-    
     return jsonify(**{"status": 200, "response": obj})
 
 @app.route("/delete/<id>", methods=['DELETE'])
@@ -53,9 +35,8 @@ def delete(id):
 @app.route("/edit_profile/<id>", methods=["PUT"])
 def update(id):
     print (id)
-    edit = {
-        "name": request.form['name'],
-        "password": request.form['password'],
+    edit = {"name": request.form['name'],
+            "password": request.form['password'],
             }
     client.db.lietotaji.update_one({"_id" : ObjectId(id)},{ "$set": edit})
     return jsonify(**{"status": 200, "response": edit})
