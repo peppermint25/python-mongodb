23ee5f2a3e98f61ba9e4ec65ffa4b230eba812aa
peppermint25 <oskars.metr01@gmail.com>
2023-05-07 22:05:36 +0300

    Added deleting from leaderboard

--- app.py
+++ app.py
@@ -25,12 +25,22 @@ def add():
     result = collection.insert_one(obj)
     return {'id': str(result.inserted_id)}
 
-@app.route("/leaderboard", methods=["GET"])
+@app.route("/leaderboard-data", methods=["GET"])
 def main():
     leaderboard = collection.find().sort([("gametime", 1)]).limit(10)
     leaderboard = list(leaderboard)
+    leaderboard.sort(key=lambda x: float(x['gametime']))
     return dumps(leaderboard)
     # return jsonify(**{"status": 200, "response": leaderboard})
+
+@app.route("/leaderboard-data/<entry_id>", methods=["DELETE"])
+def delete_user(entry_id):
+    result = collection.delete_one({"_id": ObjectId(entry_id)})
+    if result.deleted_count > 0:
+        return jsonify({"message": "User deleted from leaderboard"})
+    else:
+        return jsonify({"message": "User not found in leaderboard"})
+
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
